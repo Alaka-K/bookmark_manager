3c4218e1c1d7630b1f8ee2d0597f74934ee07979
kehinde alaka <kennyalaka93@gmail.com>
2021-12-07 14:41:02 +0000

    resolved rspec error inline with database

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -2,7 +2,7 @@ require 'bookmarks'
 describe Bookmarks do
   context "#all" do
     it "should return a list of all bookmarks" do
-      expect(Bookmarks.all).to eq "http://www.makersacademy.com/<br>http://www.google.com/<br>http://www.destroyallsoftware.com<br>"
+      expect(Bookmarks.all).to eq "http://www.makersacademy.com<br>http://www.google.com<br>http://www.destroyallsoftware.com<br>www.github.com<br>"
     end
   end
 end
